86082300c9bd22983326cf4be47b2da926565d04
Tosin2289 <ayoolatosin00@gmail.com>
2023-02-01 20:27:27 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              "assets/chef.jpg",
+              height: 400,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Enjoy Cooking",
+            style: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Start cooking with our \ngreat recipes!",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: 60,
+            width: 280,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+                child: Text(
+              "Get Strated",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            )),
+          ),
+        ],
+      ),
+    ));
   }
 }
